835643b43a043543ad8bd5b68e1392b9c2d4263e
raguk94 <kragu94kannan@gmail.com>
2025-04-16 20:48:15 +0530

    Added test git java file

--- Ragu/TestGit.java
+++ Ragu/TestGit.java
@@ -1,6 +1,12 @@
 
 public class TestGit {
 	
+	private void car() {
+		
+		System.out.println("");
+		System.out.println("");
+	}
+	
 	public static void main(String[] args) {		
 		System.out.println("Ragu");
 	}
